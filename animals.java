deac481d72d8d7437dabed37528ececb81374eb5
Burkay Tunçtürk <burkay.tuncturk@gmail.com>
2024-02-08 19:10:32 +0300

    little change

--- animals.java
+++ animals.java
@@ -1,9 +1,9 @@
 import java.util.ArrayList;
 
 public class animals {
-    ArrayList animals;
+    ArrayList<cat> animals;
 
     animals(){
-        
+        animals = new ArrayList<cat>();
     }
 }
